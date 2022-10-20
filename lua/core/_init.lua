e0de1897d9b0bb7af5f5b7261abdbd96fa9837e3
guxuerui <im.guxuerui@gmail.com>
2022-10-20 10:32:10 +0800

    fix: delete usless file

--- lua/core/_init.lua
+++ lua/core/_init.lua
@@ -37,7 +37,6 @@ vim.g.loaded_netrw             = 1
 vim.g.loaded_netrwPlugin       = 1
 vim.g.loaded_tutor_mode_plugin = 1
 vim.g.loaded_remote_plugins    = 1
---require("core.theme")
 require("core.impatient")
 require("core.plugins")
 require("core.treesitter")
